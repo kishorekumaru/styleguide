3db9727f909f513e6912d32a1cc2ab18a5bffbd6
KK <kk.kumar@tabcorp.com.au>
2020-09-01 11:03:46 +1000

    Updates

---
+++ precache-manifest.5c77b00f7dc4a198a3972c01882467aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7392add7359dbb0b64395a73478cc5f1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7066ae16b401feca7f7e.bundle.js"
+  },
+  {
+    "url": "runtime~main.7066ae16b401feca7f7e.bundle.js"
+  },
+  {
+    "revision": "005eaa0ae87c806321b9",
+    "url": "static/css/main.adcd8230.chunk.css"
+  },
+  {
+    "url": "vendors~main.7066ae16b401feca7f7e.bundle.js"
+  },
+  {
+    "url": "vendors~main.7066ae16b401feca7f7e.bundle.js.LICENSE.txt"
+  }
+]);
